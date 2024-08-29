ee48018299704b0204bf95e78813007e229ef024
cameronrtucker <ctucke31@ut.utm.edu>
2024-08-28 19:14:37 -0500

    Modified main.cpp to take a user inputted integer and add all numbers from n to 0, returning the result.

--- main.cpp
+++ main.cpp
@@ -3,8 +3,21 @@
 
 using namespace std;
 
+int sum(int n){
+  int result=0;
+  int i=1;
+  while (i<=n){
+    result+=n-(i-1);
+    ++i;
+  }
+  return result;
+}
+
 int main() {
-  cout<<"Hello World!"<<endl;
+  int n;
+  cout<<"Please enter a number: \n";
+  cin>>n;
+  cout<<sum(n)<<endl;
   
   return 0;
 }
