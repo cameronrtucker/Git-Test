d11a4172d473de742bc2011666945d4ccd63a9a4
cameronrtucker <ctucke31@ut.utm.edu>
2024-08-29 12:23:58 -0500

    Added product function which computes the product of each number from 1 to n.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,16 @@
 
 using namespace std;
 
+int product (int n){
+  int prod=1;
+  int i=1;
+  while (i<=n){
+    prod=prod * (n-(i-1));
+    ++i;
+  }
+  return prod;
+}
+
 int sum(int n){
   int result=0;
   int i=1;
@@ -18,6 +28,6 @@ int main() {
   cout<<"Please enter a number: \n";
   cin>>n;
   cout<<sum(n)<<endl;
-  
+  cout<<product(n)<<endl;
   return 0;
 }
